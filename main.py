d9da90c68170931cd83150f22730c9f4b248f5e5
anyaknopova <annilik22@gmail.com>
2023-10-14 20:35:30 +0700

    /

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 import requests
 
-r = requests.get('https://www.lamoda.ru/catalogsearch/result/?q=%D1%88%D1%82%D0%B0%D0%BD%D1%8B+%D0%BA%D0%B0%D1%80%D0%B3%D0%BE&submit=y&gender_section=women&sort=price_asc')
+url = '?'
+r = requests.get(url).text
+print(r)
