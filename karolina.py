566e4ffc9f9bdd7490cf4d2a4db29de97bd39132
Ba1an <karolina_karo_balan@mail.ru>
2023-10-15 16:42:42 +0700

    new

--- karolina.py
+++ karolina.py
@@ -6,7 +6,15 @@ query = query.lower()
 url = 'https://www.lamoda.ru/catalogsearch/result/?q='+query+'&sort=price_asc'
 
 # url = url+'&page='+ str(number)
-print(url)
 r = requests.get(url)
+
+text = r.text
+i = text.find('<a href=')
+new_text = text[i:]
+print(new_text)
+
+'''
 with open('output.txt', 'w+', encoding='utf8') as f_out:
     print(r.text, file=f_out)
+'''
+
